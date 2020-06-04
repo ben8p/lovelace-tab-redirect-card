059d96aad1df7d0813046f20c23c4703ea3df914
Benjamin Santalucia <santalub@amazon.com>
2020-06-05 00:05:53 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,47 @@
+[![hacs_badge](https://img.shields.io/badge/HACS-Custom-orange.svg?style=for-the-badge)](https://github.com/custom-components/hacs)  
+
+# tab-redirect-card
+Custom [Lovelace](https://www.home-assistant.io/lovelace) card to use in [Home assistant](https://www.home-assistant.io/) allowing you to redirect a user to certain view based on entity states.  
+You can see it as a way to set the default tab based on entity states.  
+
+A common use-case is to put the card on the default view and have it redirecting you under some conditions.  
+
+For instance:
+- if sun is below_horizon you want the default view to be the tab controlling the light
+- but when you are outside, you want the default view to be tab with the cameras
+- rest of the time, the default view should be the regular default view (aka no redirect)
+
+### Installation
+Use [HACS](https://hacs.xyz/) or follow this [guide](https://github.com/thomasloven/hass-config/wiki/Lovelace-Plugins)
+
+resources:
+  url: /local/config-template-card.js
+  type: module
+
+### Configuration example:
+Note: `redirect_to_tab` starts at 0 (first tab)  
+
+ - Redirect user "foo" to the 2nd tab if `input.binary.is_home` is `on`:
+```yaml
+type: 'custom:tab-redirect-card'
+redirect:
+ - user: 'foo'
+   entity_id: 'input.binary.is_home'
+   entity_state: 'on'
+   redirect_to_tab: 1
+```
+
+ - Redirect user "foo" to the 2nd tab if `input.binary.is_home` is `on`  
+   And redirect user "bar" to the 3rd tab if `input.binary.is_home` is `on`  
+```yaml
+type: 'custom:tab-redirect-card'
+redirect:
+ - user: 'foo'
+   entity_id: 'input.binary.is_home'
+   entity_state: 'on'
+   redirect_to_tab: 1
+ - user: 'bar'
+   entity_id: 'input.binary.is_home'
+   entity_state: 'on'
+   redirect_to_tab: 2
+```
---
+++ tab-redirect-card.js
@@ -0,0 +1,54 @@
+class TabRedirectCard extends HTMLElement {
+	set hass(hass) {
+		const homeAssistant = document.querySelector('home-assistant');
+		const root = homeAssistant.shadowRoot.querySelector('home-assistant-main').shadowRoot;
+		const sidebarRoot = root.querySelector('ha-sidebar').shadowRoot;
+		const sidebarListbox = sidebarRoot.querySelector('paper-listbox');
+		const overview = sidebarListbox.querySelector('[data-panel="lovelace"]');
+		if(!overview) { return; }
+		const panel = root.querySelector('ha-panel-lovelace')
+		if(!panel) { return; }
+		const uiRoot = panel.shadowRoot.querySelector('hui-root')
+		if(!uiRoot) { return; }
+		const header = uiRoot.shadowRoot.querySelector('app-header');
+		const isEditing = header.classList.contains('edit-mode');
+		if(isEditing) { return; }
+		const tabs = uiRoot.shadowRoot.querySelector('paper-tabs');
+		const tabList = tabs.querySelectorAll('paper-tab');
+
+		const userConfigs = this.config.redirect.filter((item) => item.user === hass.user.name);
+		userConfigs.forEach((config) => {
+			const state = hass.states[config.entity_id].state;
+			const key = `${config.user}-${config.entity_id}-${config.entity_state.toString()}`;
+			if(state === config.entity_state && sessionStorage.getItem('TabRedirectCardLastAtoRedirect') !== key) {
+				tabList[config.redirect_to_tab_index].click();
+				sessionStorage.setItem('TabRedirectCardLastAtoRedirect', key);
+			}
+		});
+	}
+
+	setConfig(config) {
+		if (!config.redirect || !Array.isArray(config.redirect) || config.redirect.length === 0) {
+			throw new Error('You need to define a redirect (array)');
+		}
+		config.redirect.forEach((redirect) => {
+			if(!redirect) {
+				throw new Error('You need to define a redirect (array)');
+			}
+			if(!redirect.user || typeof redirect.user !== 'string') {
+				throw new Error('You need to define user (string)');
+			}
+			if(!redirect.entity_id || typeof redirect.entity_id !== 'string') {
+				throw new Error('You need to define entity_id (string)');
+			}
+			if(!redirect.entity_state) {
+				throw new Error('You need to define entity_state');
+			}
+			if(!redirect.redirect_to_tab_index || typeof redirect.redirect_to_tab_index !== 'number' || !Number.isInteger(redirect.redirect_to_tab_index)) {
+				throw new Error('You need to define redirect_to_tab_index (integer, starting from 0)');
+			}
+		});
+		this.config = config;
+	}
+}
+customElements.define('tab-redirect-card', TabRedirectCard);
